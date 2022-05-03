bfb3152c1a8c8e34cf6235c3fcb5f4c6e7f6686e
Pablo <pabdom03@gmail.com>
2022-05-03 22:25:59 +0200

    favicon test 03

--- sh/git-script.sh
+++ sh/git-script.sh
@@ -3,10 +3,10 @@
 cd ~
 cd Documents/MUM/MACE/2ndo\ Bloque/Entrega/
 #/usr/bin/Rscript -e "library(knitr); knit(input = \"/home/pablo/Documents/MUM/MACE/2ndo Bloque/Entrega/source/01Fruits.Rmd\", output = \"/home/pablo/Documents/MUM/MACE/2ndo Bloque/Entrega/docs/index.html\")"
-{ /usr/bin/Rscript -e "library(knitr); rmarkdown::render(\"/home/pablo/Documents/MUM/MACE/2ndo Bloque/Entrega/source/01Fruits.Rmd\", output_format = \"html_document\")" ;} || { :;}
+{ /usr/bin/Rscript -e "library(knitr); rmarkdown::render(\"/home/pablo/Documents/MUM/MACE/2ndo Bloque/Entrega/source/01Seeds.Rmd\", output_format = \"html_document\")" ;} || { :;}
 sleep 2
 rm docs/*.html
-mv source/*.html docs/
+mv source/01Seeds.html docs/
 mv docs/*.html docs/index.html
 git add source/
 git add docs/
